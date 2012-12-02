ac90ee85ac18e7f85fc2048e9851a9a61a9f0ff6
Alexander Gryson <agryson@gmail.com>
2012-12-02 10:26:23 +0100

    fixed up a problem finding defaultWS

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('clear').addEventListener('click', clearField);
+  document.getElementById('save').addEventListener('click', cleanUp);
+  document.getElementById('settingsTitleContainer').addEventListener('click', settings);
+  init();
+});
+
+window.onunload = cleanUp();
+
 function init() {
     if(localStorage.workspace === null || localStorage.workspace === undefined) {
         document.getElementById('defaultWS').value = '';
-        startEngines('mycloud');
+        startEngines('all');
     } else {
         document.getElementById('defaultWS').value = localStorage.workspace;
         startEngines(localStorage.workspace);
@@ -10,19 +19,19 @@ function init() {
 
 
 function startEngines(workspace) {
-    var destination = 'http://mobile.hojoki.com/#stream/' + workspace; 
+    var destination = 'http://mobile.hojoki.com/#stream/' + workspace;
     var mainframe = document.getElementById('mainframe');
     mainframe.setAttribute('width', '100%');
     mainframe.setAttribute('height', '461px');
     mainframe.setAttribute('frameborder', '0');
-    mainframe.setAttribute('id', 'mainframe');
+    mainframe.setAttribute('id', 'mainframe'); //Why do I need this?
     mainframe.setAttribute('src', destination);
 }
 
 function settings() {
     var mainframe = document.getElementById('mainframe');
-    var settings = document.getElementById("settings");
-    if (parseInt(mainframe.height) == 0){
+    //var settings = document.getElementById("settings");
+    if (parseInt(mainframe.height, 10) === 0){
         mainframe.height = 461 + 'px';
     } else {
         mainframe.height = 0;
@@ -33,10 +42,10 @@ function cleanUp(closing){
     var val = '';
     var input = document.getElementById('defaultWS').value;
     for (var i = 0; i < document.getElementById('defaultWS').value.length; i++) {
-        if(input.charAt(i) !== NaN && input.charAt(i) >= 0 && input.charAt(i) <= 9){
+        if(!isNaN(input.charAt(i)) && input.charAt(i) >= 0 && input.charAt(i) <= 9){
             val += document.getElementById('defaultWS').value.charAt(i);
         }
-    };
+    }
     if (val > 0) {
         localStorage.workspace = val;
         document.getElementById('defaultWS').value = localStorage.workspace;
